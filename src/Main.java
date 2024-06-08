aef843572cf51453a988f19ab76efe7263dafc80
Robertomaker <robertovqzsdoval@gmail.com>
2024-06-07 19:38:00 -0600

    commiteando

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import evidencia.Medico;
 import evidencia.Metodos;
 
 
@@ -5,6 +6,7 @@ import java.io.*;
 
 
 import static evidencia.Metodos.medicos;
+import static evidencia.Metodos.terminarPrograma;
 import static java.lang.Integer.parseInt;
 
 
@@ -13,16 +15,19 @@ public class Main {
 
 
 
+
+        File doctores;
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int entrada = 0;
         Metodos metodos = new Metodos();
         System.out.println("Bienvenido al administrador de citas");
         metodos.AdministradorDeAcceso();
         System.out.println("Selecciona una opcion a ejecutar");
-        System.out.println("Dar de alta doctor");
-        System.out.println("Dar de alta paciente");
-        System.out.println("Crear una cita");
-        System.out.println("Mostrar citas");
+        System.out.println("Presiona 1 para: Dar de alta doctor");
+        System.out.println("Presiona 2 para :Dar de alta paciente");
+        System.out.println("Presiona 3 para :Crear una cita");
+        System.out.println("Presiona 4 para: Mostrar citas");
+        System.out.println("Presiona 5 para: Guardar los datos a archivo");
 
 
         entrada = parseInt(read.readLine());
@@ -31,6 +36,7 @@ public class Main {
             switch (entrada) {
                 case 1:
                     metodos.DarDeAltaDoctor();
+                    metodos.
                     MostrarMenu();
                     entrada = parseInt(read.readLine());
                     break;
@@ -45,19 +51,20 @@ public class Main {
                 entrada = parseInt(read.readLine());
                 break;
 
-
             }
 
         }
 
+
     }
 
     public static void MostrarMenu(){
-        System.out.println("selecciona una opcion");
-        System.out.println("Dar de alta doctor");
-        System.out.println("Dar de alta paciente");
-        System.out.println("Crear una cita");
-        System.out.println("Mostrar citas");
-
+        System.out.println("Selecciona una opcion a ejecutar");
+        System.out.println("Presiona 1 para: Dar de alta doctor");
+        System.out.println("Presiona 2 para :Dar de alta paciente");
+        System.out.println("Presiona 3 para :Crear una cita");
+        System.out.println("Presiona 4 para: Mostrar citas");
+        System.out.println("Presiona 5 para: Guardar los datos a archivo");
     }
+
 }
--- src/evidencia/Metodos.java
+++ src/evidencia/Metodos.java
@@ -11,8 +11,9 @@ public class Metodos {
     public static HashMap<String, Medico> medicos = new HashMap<String, Medico>();
     static HashMap<String, Paciente> pacientes = new HashMap<String, Paciente>();
     static HashMap<String, Cita> citas = new HashMap<String, Cita>();
+    File doctores;
 
-    static String user = "usuario";
+    static String user = "citasad";
     static String pass = "123456";
     String usuario, contraseña;
     Scanner entrada = new Scanner(System.in);
@@ -23,22 +24,30 @@ public class Metodos {
         usuario = entrada.nextLine();
         System.out.println("Ingrese la contraseña");
         contraseña = entrada.nextLine();
+
         if (user.equals(usuario) && pass.equals(contraseña)) {
             System.out.println("Datos correctos");
-
         }
-        if (pass.equals(contraseña) == false && intentos < 5) {
-            System.out.println("usuario o contraseña incorrectas, porfavor intentelo de nuevo");
+        while(pass.equals(contraseña)== false && intentos < 4 ){
+            System.out.println("Contraseña incorecta, intentelo de nuevo");
             System.out.println("Ingrese el usuario");
             usuario = entrada.nextLine();
             System.out.println("Ingrese la contraseña");
             contraseña = entrada.nextLine();
             intentos++;
-            if (intentos == 5) {
-                System.out.println("Se alcanzo el maximo de intentos, intentelo de nuevo mas tarde");
+            if (intentos == 4){
+                System.out.println("Numero de intentos permitidos alcanzado, intentelo mas tarde");
+                terminarPrograma();
             }
+
         }
 
+
+    }
+    private void EscribirArchivoDeTexto() throws IOException {
+        FileWriter escribir = new FileWriter(doctores);
+        escribir.write(medicos.entrySet().toString());
+        System.out.println("Se almaceno el doctor");
 
     }
 
@@ -50,6 +59,10 @@ public class Metodos {
         System.out.println("Mostrar citas");
 
     }
+    public static void terminarPrograma(){
+        System.out.println("Sistema bloqueado");
+        System.exit(0);
+    }
 
     public void DarDeAltaDoctor() throws IOException {
 
@@ -60,16 +73,14 @@ public class Metodos {
         medico.setNombre(entrada.nextLine());
         System.out.println("Introduce la especialidad del doctor");
         medico.setEspecialidad(entrada.nextLine());
-        System.out.println("Introduce la key del doctor");
-        String key= entrada.nextLine();
-        medicos.put(key, medico);
+        medicos.put(medico.getId(), medico);
         System.out.println("Se añadio el doctor");
         System.out.println(medicos.entrySet());
 
-
-
-
     }
+
+
+
 
     public void DarDeAltaPaciente() {
         Paciente paciente = new Paciente();
@@ -77,17 +88,16 @@ public class Metodos {
         paciente.setId(entrada.nextLine());
         System.out.println("Introduce el nombre del paciente");
         paciente.setNombre(entrada.nextLine());
-        System.out.println("Introduce la key del paciente");
-        String key= entrada.nextLine();
-        pacientes.put(key, paciente);
+        pacientes.put(paciente.getId(), paciente);
         System.out.println("Se añadio el paciente");
         System.out.println(pacientes.entrySet());
 
     }
 
+
+
     public void CrearCita() {
         Cita cita = new Cita();
-
         System.out.println("Introduce el id de la cita");
         cita.setId(entrada.nextLine());
         System.out.println("Introduce la fecha y hora de la cita");
@@ -95,14 +105,13 @@ public class Metodos {
         System.out.println("Introduce el motivo de la cita");
         cita.setMotivo(entrada.nextLine());
         System.out.println("Introduce el medico tratante");
-        Medico medico = medicos.get(entrada.nextLine());
-        cita.setId(String.valueOf(medico));
+        cita.setMedico(medicos.get(entrada.nextLine()));
         System.out.println("Introduce el paciente");
-        Paciente paciente = pacientes.get(entrada.nextLine());
-        cita.setId(String.valueOf(paciente));
-        citas.put("paciente: ", cita);
+        cita.setPaciente(pacientes.get(entrada.nextLine()));
+        citas.put(cita.getId(), cita);
         System.out.println("Se creo la cita");
         System.out.println(citas.entrySet());
 
     }
+
 }
